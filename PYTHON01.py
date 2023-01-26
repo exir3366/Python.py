bef4291b0e16fa116cd40f591ed9412ba652d232
Ruderick Veve <116129374+exir3366@users.noreply.github.com>
2023-01-25 18:23:13 -0800

    Create PYTHON01.py
    Added formatting code to final output

--- PYTHON01.py
+++ PYTHON01.py
@@ -1,4 +1,4 @@
 farenheit = float(input ( "Enter temperature in farenheit" ))
 kelvin = (farenheit + 459.67) * 5/9
-print ('Here is the temperature in Kelvin:')
+print ('Here is the temperature in Kelvin:' / format(kelvin, '.2f'))
 print ('kelvin:', kelvin)
